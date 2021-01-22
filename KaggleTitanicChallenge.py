eb8308231010d50ce8def623b1550651ec86c9f4
Sam Whittington <69531345+SamWhittington@users.noreply.github.com>
2021-01-22 18:30:06 +0000

    Update KaggleTitanicChallenge.py

--- KaggleTitanicChallenge.py
+++ KaggleTitanicChallenge.py
@@ -1,10 +1,11 @@
-import codecademylib3_seaborn
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 
 # Load the passenger data
 passengers = pd.read_csv('passengers.csv')
@@ -36,33 +37,52 @@ survival = passengers['Survived']
 
 # Perform train, test, split
 
-train_test_split(features,labels,test_size = 0_to_1_test_size)
+train_features, test_features, train_labels, test_labels = train_test_split(features, survival,  test_size=0.2)
+print(train_features)
 
-# Scale the feature data so it has mean = 0 and standard deviation = 1
+# Scale the feature data so it has mean = 0 and standard deviation = 1 i.e. Normalise the data:
 
+
+scaler = StandardScaler()
+train_features = scaler.fit_transform(train_features)
+test_features = scaler.transform(test_features)
 
 # Create and train the model
+
+model = LogisticRegression()
+model.fit(train_features, train_labels)
 
 
 # Score the model on the train data
 
+print(model.score(train_features, train_labels))
 
 # Score the model on the test data
 
+print(model.score(test_features, test_labels))
 
 # Analyze the coefficients
 
+print(model.coef_)
+
+print(list(zip(['Sex','Age','FirstClass','SecondClass'],model.coef_[0])))
 
 # Sample passenger features
 Jack = np.array([0.0,20.0,0.0,0.0])
 Rose = np.array([1.0,17.0,1.0,0.0])
-# You = np.array([___,___,___,___])
+
+You = np.array([1.0,38.0,0.0,0.0])
+print(You)
 
 # Combine passenger arrays
-
+sample_passengers =  np.array([Jack, Rose, You])
 
 # Scale the sample passenger features
 
+sample_passengers = scaler.transform(sample_passengers)
+print(sample_passengers)
 
 # Make survival predictions!
 
+print(model.predict(sample_passengers))
+print(model.predict_proba(sample_passengers))
